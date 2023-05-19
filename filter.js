bd8b2e807df47ab913c8563b80e8697a16fb20fc
Eyituoyo George Ikomi <ikomieyituoyogeorge@yahoo.com>
2023-05-19 12:46:45 +0000

    Create filter.js for practice

--- filter.js
+++ filter.js
@@ -21,6 +21,12 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+let result = people.filter(person => person.name === "Paul");
+console.log(result);
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +61,9 @@ const students = [
     ]
   },
 ];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+
+console.log(candidates);
